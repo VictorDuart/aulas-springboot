5f8863a7cae6e9b0fee2420ba8c70d2682f76923
VictorDuart <victorhd2014@gmail.com>
2022-11-05 17:15:43 -0300

    create profiles: dev and prod

--- src/main/java/io/github/VictorDuart/AppConfiguration.java
+++ src/main/java/io/github/VictorDuart/AppConfiguration.java
@@ -1,13 +1,18 @@
 package io.github.VictorDuart;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 
 @Configuration
+@Profile("dev")
 public class AppConfiguration {
 
     @Bean(name = "applicationName")
-    public String applicationName(){
-        return "Sales App";
+    public CommandLineRunner execute(){
+        return args -> {
+            System.out.println("Rodando config dev");
+        };
     }
 }
