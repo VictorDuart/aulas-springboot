4f54d25b1c98103c084c87991696aff69e485a31
VictorDuart <victorhd2014@gmail.com>
2022-11-05 16:48:51 -0300

    create application.properties

--- src/main/java/io/github/VictorDuart/SalesApp.java
+++ src/main/java/io/github/VictorDuart/SalesApp.java
@@ -2,6 +2,7 @@ package io.github.VictorDuart;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
@@ -12,8 +13,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class SalesApp {
 
-    @Autowired
-    @Qualifier("applicationName")
+    @Value("${application.name}")
     private String applicationName;
 
     @GetMapping("/hello")
